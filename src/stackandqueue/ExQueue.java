229fafac2c8728a2f814aa713a36786fe234bed1
Yunus Sezgin <yunussezgin8@hotmail.com>
2022-03-16 03:32:00 +0300

    Add queue and stack

--- src/stackandqueue/ExQueue.java
+++ src/stackandqueue/ExQueue.java
@@ -1,5 +1,34 @@
 package stackandqueue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ExQueue {
 
+	public static void main(String[] args) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(1);
+		queue.add(2);
+		queue.add(3);
+		queue.add(4);
+		queue.add(5);
+
+		while (!queue.isEmpty()) {
+			System.out.println(queue.remove());
+		}
+
+		System.out.println(queue.size());
+
+		System.out.println(queue.isEmpty());
+
+		queue.add(6);
+		queue.add(7);
+		queue.add(8);
+
+		for (Integer q : queue) {
+			System.out.println(q);
+		}
+
+		System.out.println(queue.peek());
+	}
 }
--- src/stackandqueue/ExStack.java
+++ src/stackandqueue/ExStack.java
@@ -1,10 +1,30 @@
 package stackandqueue;
 
+import java.util.Stack;
+
 public class ExStack {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Stack<Integer> stack = new Stack<>();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
+		stack.push(5);
 
+		while (!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
+
+		System.out.println(stack.size());
+
+		System.out.println(stack.isEmpty());
+
+		stack.push(6);
+		stack.push(7);
+		stack.push(8);
+
+		System.out.println(stack.peek());
 	}
 
 }
